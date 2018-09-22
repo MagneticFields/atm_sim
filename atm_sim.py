95f1d3096c498f0ec3b2074dc4fbd75eab6ce1ed
Alper <aarslan@gmail.com>
2018-09-22 18:36:46 +0300

    accounts dict is saving to a file with pickle, implemented some functions

--- atm_sim.py
+++ atm_sim.py
@@ -1,9 +1,8 @@
 """ Simple atm simulator"""
-
-
-
-accounts = {} # This dict is for storing account ids and passwords. Keys = id, values = pass
-
+import pickle
+with open("accounts.pickle", "rb") as file:
+    accounts = pickle.load(file)
+file.close()
 
 
 def main_menu():
@@ -16,16 +15,16 @@ def main_menu():
     print("--------------------------------------")
     print("1) Log in to your  account\n")
     print("2) Create an account\n")
+    print("3) Exit\n")
     resp = input("Please select an option: ")
-    if resp != "1" and resp != "2":
-        print("Please enter a valid option")
+    if resp != "1" and resp != "2" and resp != "3":
+        print("Please enter a valid option\n")
         main_menu()
     else:
         return resp
 
 
 def login_menu():
-
     pass
 
 
@@ -42,40 +41,51 @@ def open_account():
         month = now.month
         id_seed = 1
         generated_acc_id = str(year) + str(month) + str(id_seed)
-        id_seed += 1
+        while generated_acc_id in accounts:
+            id_seed += 1
+            generated_acc_id = str(year) + str(month) + str(id_seed)
         return generated_acc_id
 
     acc_id = account_id_generator()
     print("Your account id is: {}".format(acc_id))
     passwd = input("Please enter a password for your account: ")
     accounts[acc_id] = passwd
-    print("Your account has successfully created")
-    main_menu()
-
-
+    print("Your account has successfully created\n")
+    print(accounts)
+    main()
 
 
 def save_accounts():
     """This function saves the account list to a file"""
-    # TODO
+    with open("accounts.pickle", "wb") as file:
+        pickle.dump(accounts, file, protocol=pickle.HIGHEST_PROTOCOL)
+    file.close()
 
 
 def log_in():
-    # TODO
-    print("Log In")
+    acc_num = input("Please enter your account number\n")
+    pwd = input("Please enter your password\n")
+
+    while
+
+    pass
 
 
-def load_accounts():
-    """ This helper function loads account objects from list created by open account function"""
+def end_program():
+    save_accounts()
+    print("Thank yu for using Jupiter Bank ATM Machine\n")
+    print("We wish you a pleasant day :) \n")
 
 
 def main():
-    load_accounts()
     response = main_menu()
     if response == "1":
         log_in()
     elif response == "2":
         open_account()
+    elif response == "3":
+        end_program()
+
 
 
 if __name__ == "__main__":
